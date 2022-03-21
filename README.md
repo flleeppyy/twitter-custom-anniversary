a693abf064855714da89c7aaf052494ce732ccfa
flleeppyy <flleeppyy@gmail.com>
2022-03-20 23:49:38 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Twitter Custom anniversary Bot
---
+++ index.js
@@ -0,0 +1,8 @@
+// https://twitter.com/i/notifications/anniversary
+//  ?title=Happy%20Twitter%20anniversary
+//  &message=You%20joined%205%20years%20ago%20today!%20Share%20the%20big%20day%20with%20others%20in%20your%20Twitter%20community.
+//  &action=Celebrate%20with%20a%20Tweet
+//  &text=Do%20you%20remember%20when%20you%20joined%20Twitter%3F%20I%20do!%20%23MyTwitterAnniversary
+//  &image_attachment=https%3A%2F%2Fton.twimg.com%2Fntab_public%2Ftwitterversary%2Fyear5.jpg
+//  &image_width=1920&image_height=1080
+
